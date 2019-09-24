f402c2b1246c631803bd306c3de7bb744eb489d3
zouchanglin <zchanglin@aliyun.com>
2019-09-24 14:34:51 +0800

    update code

--- src/main/java/edu/xpu/buckmoo/BuckmooApplication.java
+++ src/main/java/edu/xpu/buckmoo/BuckmooApplication.java
@@ -3,11 +3,10 @@ package edu.xpu.buckmoo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Maven的配置问题导致的工程导入出错
 @SpringBootApplication
 public class BuckmooApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BuckmooApplication.class, args);
     }
-
 }
